017ada2eedea98b46dbc726f5b953964c7d9828f
Hasan <46130250+hasan-mustafaa@users.noreply.github.com>
2024-05-17 14:29:34 -0400

    Removed Redundancy

--- Main Project/Scenes/Characters/Enemies/patrolling_enemy.gd
+++ Main Project/Scenes/Characters/Enemies/patrolling_enemy.gd
@@ -1,44 +1,17 @@
 extends CharacterBody2D
 
-#FLip if it touches a wall
+@onready var sprite_2d = $AnimatedSprite2D
+@export var group_name : String
 
-var SPEED = 150.0
+var SPEED = 175.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_right = true
-var startPosition
-var endPosition
-
-@onready var sprite_2d = $AnimatedSprite2D
-@export var limit = 0.5
-@export var xOffset = 25
-@export var raycastDistance = 37
-
-
-
-func _ready():
-	startPosition = position
-	endPosition = startPosition + Vector2(0, 3*16)
-
-func changeDirection():
-	var tempEnd = endPosition
-	endPosition = startPosition
-	startPosition = tempEnd
-
-func updateVelocity():
-	var moveDirection = endPosition - position
-	if moveDirection.length() < limit:
-		changeDirection()
-	velocity = moveDirection.normalized*SPEED
 
 func _physics_process(delta):
 	#Animations
 	sprite_2d.animation = 'running'
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	if !$RayCast2D.is_colliding() && is_on_floor():
-		flip()
-		
 	velocity.x = SPEED
 	move_and_slide()
 	handleCollision()
@@ -47,18 +20,7 @@ func handleCollision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
-
-
-func flip():
-	#Flips the player
-	facing_right = !facing_right
-	scale.x = abs(scale.x) + -1
-	if facing_right:
-		SPEED = abs(SPEED)
-	else:
-		SPEED = abs(SPEED) * -1
-
-
+		
 func _on_hurtbox_area_entered(area):
 	if area == $"Hitbox": return
 	sprite_2d.self_modulate = Color("red")
