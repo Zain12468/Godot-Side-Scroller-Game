22e657f430c1963283c19ff3b0545f756c62c267
Zain12468 <zain.s.hameed@gmail.com>
2024-05-17 14:02:37 -0400

    Player and Enemy bugfixes
    Fix player jump height. Also fixed enemy hitbox.

--- Main Project/Scenes/Characters/Player/main_character.gd
+++ Main Project/Scenes/Characters/Player/main_character.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 signal healthChanged
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = -750.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
